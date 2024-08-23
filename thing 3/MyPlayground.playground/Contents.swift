781e622170144c0cd24f6ed4a132c8de2021400a
River <river.johnson@stu.barren.kyschools.us>
2024-08-23 08:51:30 -0500

    Thing 3 Done

---
+++ thing 3/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+var i = 0
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    }
+    else if i.isMultiple(of: 3) {
+        print("Fizz")
+    }
+    else if i.isMultiple(of: 5) {
+        print("Buzz")
+    }
+    else {
+        print(i)
+    }
+}
