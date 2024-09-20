5f4c5efcf7058c0c60ede60111e1dc01f89baf20
rjohn11118 <145144057+rjohn11118@users.noreply.github.com>
2024-09-20 08:16:16 -0500

    Update Contents.swift

--- pg9/MyPlayground.playground/Contents.swift
+++ pg9/MyPlayground.playground/Contents.swift
@@ -1,3 +1,11 @@
-import UIKit
+func getRandomNumber(from array: [Int]?) -> Int {
+    array?.randomElement() ?? Int.random(in: 1...100)
+}
 
-var greeting = "Hello, playground"
+let numbers1: [Int]? = [5, 10, 15, 20]
+let numbers2: [Int]? = nil
+let numbers3: [Int]? = []
+
+print("Random number from numbers1: \(getRandomNumber(from: numbers1))")
+print("Random number from numbers2: \(getRandomNumber(from: numbers2))")
+print("Random number from numbers3: \(getRandomNumber(from: numbers3))")
